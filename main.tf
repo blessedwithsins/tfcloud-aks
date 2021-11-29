640b6eb88ca5a467a058d8633c7a7b248e7ebc3a
blessedwithsins <uchihamadara1503@gmail.com>
2021-11-29 18:57:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
-provider "azurerm" {
-  version = "=2.46.0"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
 }
-
 resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
   location = var.location
