09369117c4b53bc3c1e6ae124524b241cafa2672
blessedwithsins <uchihamadara1503@gmail.com>
2021-12-02 19:40:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,5 +29,12 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   }
 }
 
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.tfcloud.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.tfcloud.kube_config.0.client_certificate)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.tfcloud.kube_config.0.cluster_ca_certificate)
+}
+
+
 
 
