bb15a83e09350aca06e0805cc44b23e8762553a8
blessedwithsins <uchihamadara1503@gmail.com>
2021-12-02 19:06:22 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,8 +0,0 @@
-provider "kubernetes" {
-  host                   = data.azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "az"
-    args        = ["aks", "get-credentials", "--name", "tfcloud", "--resource-group", "tfcloud"]
-  }
