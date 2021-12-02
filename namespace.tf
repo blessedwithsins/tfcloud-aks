18d3a72962c5932cec79e5a2bacd6893ff7d17dc
blessedwithsins <uchihamadara1503@gmail.com>
2021-12-02 19:39:53 +0530

    Update namespace.tf

--- namespace.tf
+++ namespace.tf
@@ -1,8 +1,3 @@
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.tfcloud.kube_config.0.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.tfcloud.kube_config.0.client_certificate)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.tfcloud.kube_config.0.cluster_ca_certificate)
-}
 
 resource "kubernetes_namespace" "example" {
   metadata {
