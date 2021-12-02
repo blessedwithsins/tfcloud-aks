fdccefeb67c861ccdc6b8419a504e57f8008e2d7
blessedwithsins <uchihamadara1503@gmail.com>
2021-12-02 20:09:53 +0530

    Update namespace.tf

--- namespace.tf
+++ namespace.tf
@@ -1,3 +1,17 @@
+data "azure_aks_cluster" "aks_cluster" {
+  name = var.cluster_name
+}
+
+data "azure_aks_cluster_auth" "aks_cluster_auth" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.azure_aks_cluster.aks_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.azure_aks_cluster.aks_cluster.certificate_authority.0.data)
+  token                  = data.azure_aks_cluster_auth.aks_cluster_auth.token
+  load_config_file       = false
+}
 
 resource "kubernetes_namespace" "example" {
   metadata {
