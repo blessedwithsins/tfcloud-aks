dc007842c7d545a2eaaf63da1b94b40bece4e010
blessedwithsins <uchihamadara1503@gmail.com>
2021-12-02 19:04:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,3 +28,12 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     Environment = var.environment
   }
 }
+
+provider "kubernetes" {
+  host                   = data.azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "az"
+    args        = ["aks", "get-credentials", "--name", "tfcloud", "--resource-group", "tfcloud"]
+  }
