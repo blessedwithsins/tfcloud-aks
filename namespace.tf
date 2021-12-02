3b122bc99518b595e5ef33f5cc9c94b70cd28dcc
blessedwithsins <uchihamadara1503@gmail.com>
2021-12-02 19:28:42 +0530

    Update namespace.tf

--- namespace.tf
+++ namespace.tf
@@ -1,18 +1,3 @@
-data "azurerm_kubernetes_cluster" "aks_cluster" {
-  name = "tfcloud"  
-  resource_group_name = "tfcloud" 
-}
-
-provider "kubernetes" {
-  host                   = data.azurerm_kubernetes_cluster.aks_cluster.name.kube_config.0.host
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks_cluster.name.kube_config.0.cluster_ca_certificate)
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "az"
-    args        = ["aks", "get-credentials", "--name", "tfcloud", "--resource-group", "tfcloud"]
-  }
-}
-
 resource "kubernetes_namespace" "example" {
   metadata {
     annotations = {
